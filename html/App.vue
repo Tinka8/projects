f9f34356f34d18e2526e4bcb1021423c73840133
Tinka8 <lenmatka818@gmail.com>
2023-06-07 13:50:43 +0200

    index

--- html/App.vue
+++ html/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <main class="container mx-auto text-center">
+  <main id="app" class="container mx-auto text-center">
     <div>
       <ArrowSmallDownIcon />
     </div>
-    <div class="grid grid-cols-5 space-x-10 py-12">
-      <div v-for="(day, index) in days" :key="index" class="shadow-xl bg-white">
+    <div class="grid grid-cols-5 space-x-10 py-2">
+      <div v-for="(day, index) in days" :key="index" class="bg-white">
           <div class="text-lg font-bold p-2 border-b">{{ day }}</div>
 
       </div>
     </div>
 
-    <div class="grid grid-cols-5 space-x-10 py-12">
+    <div class="grid grid-cols-5 space-x-10 py-2">
       <div v-for="(date, index) in dates" :key="index">
-              <div class="p-2">{{ date }}</div>
-              <div class="bg-blue-100 space-y-2">
+            <div class="p-2">{{ date }}</div>
+          <div class="bg-blue-100 space-y-2">
               <div v-for="(timeSlot, index) in timeSlots" :key="index" class="p-8 rounded-lg">
                 <div class="text-2xl py-2 rounded-lg">{{ timeSlot }}</div>
               </div>
@@ -21,6 +21,7 @@
       </div>
     </div>
 
+    <div v-if="selected" @click="">{{ date }} {{ day }} {{ timeSlots }}</div>
 
   </main>
 </template>
@@ -36,5 +37,5 @@ const timeSlots = ref(["8:00", "9:00", "10:00", "11:00"]);
 const dates = ref(["6.jul", "7.jul", "8.jul", "9.jul", "10.jul"]);
 
 
-
+  
 </script>
