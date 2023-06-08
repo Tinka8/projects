22c18276307eb6a9b4d7d3aac66245165c3cd7f7
Tinka8 <lenmatka818@gmail.com>
2023-06-08 10:24:59 +0200

    index

--- html/App.vue
+++ html/App.vue
@@ -1,40 +1,24 @@
 <template>
-  <main id="app" class="container mx-auto text-center">
-    <div>
-      <ArrowSmallDownIcon />
-    </div>
-    <div class="grid grid-cols-5 space-x-10 py-2">
-      <div v-for="(day, index) in days" :key="index" class="bg-white">
-          <div class="text-lg font-bold p-2 border-b">{{ day }}</div>
-
+  <main>
+    <div class="relative h-64 w-64 left-1/3 py-10">
+      <div class="bg-blue-100 rounded text-center p-6">
+        <h4 class="text-lg font-semibold">
+          The current count is...
+        </h4>
+        <h1 class="p-2 text-3xl font-semibold">{{ count }}</h1>
+        <button @click="count--" class="border border-1 border-gray-200 shadow-xl rounded-lg bg-white px-6 m-4">-</button>
+        <button @click="count++" class="border border-1 border-gray-200 shadow-xl rounded-lg bg-white px-6 m-4">+</button>
       </div>
     </div>
-
-    <div class="grid grid-cols-5 space-x-10 py-2">
-      <div v-for="(date, index) in dates" :key="index">
-            <div class="p-2">{{ date }}</div>
-          <div class="bg-blue-100 space-y-2">
-              <div v-for="(timeSlot, index) in timeSlots" :key="index" class="p-8 rounded-lg">
-                <div class="text-2xl py-2 rounded-lg">{{ timeSlot }}</div>
-              </div>
-        </div>
-      </div>
-    </div>
-
-    <div v-if="selected" @click="">{{ date }} {{ day }} {{ timeSlots }}</div>
-
   </main>
 </template>
 
 
 
 <script setup>
-import { ref } from "vue";
-import { ArrowSmallUpIcon, ArrowSmallDownIcon } from '@heroicons/vue/24/solid';
+  import { ref } from "vue";
 
-const days = ref(["Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek"]);
-const timeSlots = ref(["8:00", "9:00", "10:00", "11:00"]);
-const dates = ref(["6.jul", "7.jul", "8.jul", "9.jul", "10.jul"]);
+  const count = ref(0);
 
 
   
